6a2ffc40e8f557c1d4c5987104ffd046beed0eac
Igor <azikov@ascsoft.ru>
2020-10-01 10:27:47 +0300

    Initial code

---
+++ password_check.py
@@ -0,0 +1,60 @@
+import requests
+import hashlib
+import sys
+
+
+
+def filterbyvalue(seq, value):
+   '''
+   Hash filteg generator
+   :param seq:
+   :param value:
+   :return:
+   '''
+   for el in seq:
+       if el[0]==value:
+           yield el[1]
+
+def request_api_data(part_hash):
+    '''
+    Requests hashes tails from url
+    :param part_hash: string - first 5 characters of hash
+    :return:
+    '''
+
+    url = 'https://api.pwnedpasswords.com/range/' + part_hash
+    res = requests.get(url)
+    if res.status_code != 200:
+        raise RuntimeError(f'Error resolving adress {url}')
+    return res
+
+def get_pwd_leaks_count(hashes, hash_to_check):
+    hashes = (line.split(':') for line in hashes.text.splitlines())
+    res = [i for i in filterbyvalue(hashes, hash_to_check)]
+    if len(res):
+        return res[0]
+    return 0
+
+def password_chk(password):
+    '''
+    Hashes password by SHA1 algorithm
+    :param password: string
+    :return:
+    '''
+
+    sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
+    first5_char, tail = sha1password[:5], sha1password[5:]
+    response = request_api_data(first5_char)
+    return get_pwd_leaks_count(response, tail)
+
+def main(args):
+    for passw in args:
+        count = password_chk(passw)
+        if count:
+            print(f'{passw} was found {count} times')
+        else:
+            print(f'{passw} is strong enough')
+
+if __name__ = '__main__':
+    main(sys.argv[1:])
+   # sys.exit(main(sys.argv[1:])) - принудительный выход если что-то пошло не так и зависло
